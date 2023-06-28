fe80f82f4d2d730a78703930df97311c27ba8195
Dongwoongkim <rlaehddnd0422@naver.com>
2023-06-28 16:13:57 +0900

    i18n

--- src/main/java/dongwoongkim/springbootboard/advice/ExceptionAdvice.java
+++ src/main/java/dongwoongkim/springbootboard/advice/ExceptionAdvice.java
@@ -15,151 +15,180 @@ import dongwoongkim.springbootboard.exception.member.MemberNotFoundException;
 import dongwoongkim.springbootboard.exception.message.MessageNotFoundException;
 import dongwoongkim.springbootboard.exception.post.PostNotFoundException;
 import dongwoongkim.springbootboard.exception.role.RoleNotFoundException;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.LocaleContextResolver;
+import org.springframework.web.servlet.LocaleResolver;
 
 @RestControllerAdvice
+@RequiredArgsConstructor
 @Slf4j
 public class ExceptionAdvice {
+
+    private final MessageSource ms;
 
     // uncatched Exception
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public Response exception(Exception e) {
         log.info("e = {}", e.getMessage());
-        e.printStackTrace();
-        return Response.failure(500, "오류가 발생하였습니다");
+        return getFailureResponse("exception.code", "exception.msg");
     }
 
     // Field Error
     @ExceptionHandler(BindException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public Response bindException(BindException e) {
-        return Response.failure(400, "양식에 맞게 입력해주세요.");
+        return getFailureResponse("exception.code.bad_request", "exception.msg.binding");
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public Response methodArgumentNotValidException(MethodArgumentNotValidException e) {
-        return Response.failure(400, "양식에 맞게 입력해주세요.");
+        return getFailureResponse("exception.code.bad_request", "exception.msg.binding");
     }
 
     // auth
     @ExceptionHandler(IllegalAuthenticationException.class)
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
     public Response illegalAuthenticationException(IllegalAuthenticationException e) {
-        return Response.failure(401, "유효하지 않은 권한입니다.");
+        return getFailureResponse("exception.code.authorize", "exception.msg.authorize");
     }
 
     @ExceptionHandler(AccessDeniedException.class)
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
     public Response accessDeniedException(AccessDeniedException e) {
-        return Response.failure(401, "접근 불가능한 권한 입니다.");
+        return getFailureResponse("exception.code.authorize", "exception.msg.authorize");
     }
-
 
     @ExceptionHandler(AuthenticationEntryPointException.class)
     @ResponseStatus(HttpStatus.FORBIDDEN)
     public Response authenticationEntryPointException(AuthenticationEntryPointException e) {
-        return Response.failure(403, "인증되지 않은 사용자입니다.");
+        return getFailureResponse("exception.code.forbidden", "exception.msg.un_login");
     }
 
     @ExceptionHandler(LoginFailureException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public Response loginFailureException(LoginFailureException e) {
-        return Response.failure(404, "로그인에 실패햐였습니다.");
+        return getFailureResponse("exception.code.bad_request", "exception.msg.login");
     }
 
     @ExceptionHandler(ValidateTokenException.class)
-    @ResponseStatus(HttpStatus.NOT_ACCEPTABLE)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public Response validateTokenException(ValidateTokenException e) {
-        return Response.failure(406, "비정상적인 시도입니다.");
+        return getFailureResponse("exception.msg.validate_token", "exception.msg.binding");
+
     }
 
     // category
     @ExceptionHandler(CannotConvertNestedStructureException.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public Response cannotConvertNestedStructureException(CannotConvertNestedStructureException e) {
-        return Response.failure(500, "목록을 계층형 구조로 변경할 수 없습니다.");
+        return getFailureResponse("exception.code", "exception.msg.category.convert");
+
     }
 
     @ExceptionHandler(CategoryNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public Response categoryNotFoundException(CategoryNotFoundException e) {
-        return Response.failure(404, "카테고리를 찾을 수 없습니다.");
+        return getFailureResponse("exception.code.not_found", "exception.msg.category.notfound");
     }
 
     // comment
     @ExceptionHandler(CommentNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public Response commentNotFoundException(CommentNotFoundException e) {
-        return Response.failure(404, "해당 댓글을 찾을 수 없습니다.");
+        return getFailureResponse("exception.code.not_found", "exception.msg.comment.notfound");
     }
-
 
     // image
     @ExceptionHandler(CannotFindExtException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public Response cannotFindExtException(CannotFindExtException e) {
-        return Response.failure(404, "업로드할 파일의 확장자를 찾을 수 없습니다.");
+        return getFailureResponse("exception.code.bad_request", "exception.msg.file.ext.notfound" );
+
     }
     @ExceptionHandler(FileUploadFailureException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public Response fileUploadFailureException(FileUploadFailureException e) {
-        return Response.failure(404, "파일 업로드에 실패햐였습니다.");
+        return getFailureResponse("exception.code.bad_request", "exception.msg.file.uploadFail");
+
     }
     @ExceptionHandler(UnsupportedImageFormatException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public Response unsupportedImageFormatException(UnsupportedImageFormatException e) {
-        return Response.failure(404, "지원하지 않는 파일형식 입니다.");
+        return getFailureResponse("exception.code.bad_request", "exception.msg.file.un_support");
     }
 
     // member
     @ExceptionHandler(DuplicateEmailException.class)
     @ResponseStatus(HttpStatus.CONFLICT)
     public Response duplicateEmailException(DuplicateEmailException e) {
-        return Response.failure(409, "중복된 Email입니다.");
+        return getFailureResponse("exception.code.conflict", "exception.msg.member.duplicate.email");
+
     }
+
     @ExceptionHandler(DuplicateUsernameException.class)
     @ResponseStatus(HttpStatus.CONFLICT)
     public Response duplicateUsernameException(DuplicateUsernameException e) {
-        return Response.failure(409, "중복된 Username입니다.");
+        return getFailureResponse("exception.code.conflict", "exception.msg.member.duplicate.username");
+
     }
     @ExceptionHandler(InputFormException.class)
     @ResponseStatus(HttpStatus.NOT_ACCEPTABLE)
     public Response inputFormException(InputFormException e) {
-        return Response.failure(406, "회원가입 양식에 맞기 입력해주세요.");
+        return getFailureResponse("exception.code.not_acceptable", "exception.msg.binding");
     }
+
     @ExceptionHandler(MemberNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public Response memberNotFoundException(MemberNotFoundException e) {
-        return Response.failure(404, "요청한 회원을 찾을 수 없습니다.");
+        return getFailureResponse("exception.code.not_found", "exception.msg.member.notfound");
+
     }
 
     // message
     @ExceptionHandler(MessageNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public Response messageNotFoundException(MessageNotFoundException e) {
-        return Response.failure(400, "해당 쪽지를 찾을 수 없습니다.");
+        return getFailureResponse("exception.code.not_found", "exception.msg.message.notfound");
     }
 
     // post
     @ExceptionHandler(PostNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public Response postNotFoundException(PostNotFoundException e) {
-        return Response.failure(400, "해당 게시물을 찾을 수 없습니다.");
+        log.info("lang = {}", LocaleContextHolder.getLocale());
+        log.info("ms = {}", ms.getClass());
+        return getFailureResponse("exception.code.not_found", "exception.msg.post.notfound");
     }
 
     // role
     @ExceptionHandler(RoleNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public Response roleNotFoundException(RoleNotFoundException e) {
-        return Response.failure(404, "요청한 권한 등급을 찾을 수 없습니다.");
+        return getFailureResponse("exception.code.not_found", "exception.msg.role.notfound");
+    }
+
+
+    private Response getFailureResponse(String code, String msg) {
+        return Response.failure(getCode(code),getMessage(msg));
+    }
+
+    private Integer getCode(String code) {
+      return Integer.valueOf(ms.getMessage(code,null,null));
+    }
+
+    private String getMessage(String msg) {
+        return ms.getMessage(msg, null, LocaleContextHolder.getLocale());
     }
 }
 
